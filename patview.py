58abb177dbbd3fd461b662066b9ac4fc3194eba6
torhen <torhen@gmail.com>
2025-06-21 17:23:54 +0200

    Add files via upload

--- patview.py
+++ patview.py
@@ -59,7 +59,213 @@ def read_header(msi_path):
                 return l
     return l
 
+class FolderBrowser(ttk.Treeview):
+    def __init__(self, parent_window, file_list):
+        super().__init__(parent_window)
+        self.mainwindow = parent_window
+        self.file_list = file_list
+        self.files = []
 
+        self.bind('<<TreeviewOpen>>', self.add_sub_folders)
+        self.bind("<<TreeviewSelect>>", self.get_and_add_files)
+
+
+    def set_folder(self, folder):
+        path_parts = folder.parts
+        item = ''
+        for i in range(1, len(path_parts) + 1):
+            full_path = pathlib.Path(*path_parts[0:i])
+            if  full_path.name == '':
+                entry_text = full_path
+            else:
+                entry_text = full_path.name
+            
+            item = self.insert(item, 'end', str(full_path), text=entry_text)
+            self.item(item, open=True)
+
+    def add_sub_folders(self, e):
+        base = self.focus()
+        for item in pathlib.Path(base).iterdir():
+            if item.is_dir() and str(item) not in self.get_children(base):
+                self.insert(base, 'end', str(item), text=item.name)
+
+    def get_and_add_files(self, e):
+        folder = self.focus()
+        self.file_list.get_files(folder)
+
+
+class FileList(ttk.Treeview):
+    def __init__(self, parent_window, drawing):
+        self.files = []
+        self.parent_window = parent_window
+        self.drawing = drawing
+        super().__init__(parent_window)
+
+        self['columns'] = ('freq', 'tilt')
+        self.heading('freq', text='Frequency')
+        self.heading('tilt', text='Tilt')
+        self.config(selectmode='extended')
+        self.heading('#0', text='filename')
+        self.heading('freq', text='freq')
+        self.heading('tilt', text='tilt')
+        self.column('#0', width=300, anchor='w')
+        self.column('tilt', width=10, anchor='w')
+        self.column('freq', width=20, anchor='w')
+        self.bind("<<TreeviewSelect>>", self.draw)
+        # self.table.bind('<Button-1>', self.on_treeview_click)
+
+    def get_files(self, folder):
+        # delete all entries
+        for item in self.get_children():
+            self.delete(item)
+
+        for item in pathlib.Path(folder).iterdir():
+            if item.is_file():
+                r = re.match(r'.*_(-\d|\d\d)T.*', item.name)
+                if r:
+                    tilt = r.group(1)
+                else:
+                    tilt = ''
+                r = re.match(r'.*_(\d{3,4})_', item.name)
+                if r:
+                    freq = r.group(1)
+                else:
+                    freq = ''
+
+                row = {
+                    'path' :str(item),
+                    'name' : item.name,
+                    'freq' : freq,
+                    'tilt' : tilt
+                }
+
+                self.insert('', 'end', row['path'], text=row['name'], values= [row['freq'], row['tilt'] ])
+
+    # def sort_files(self, columns, ascending):
+    #     self.files = sorted(self.files, key=lambda row: row[columns], reverse= not ascending)
+
+
+    def draw(self, e):
+        self.drawing.draw(self.selection())
+ 
+ 
+class Drawing(tk.Canvas):
+    def __init__(self, parent_window):
+        self.parent_window = parent_window
+        self.fontname = "Consolas"
+        self.fontsize = 10
+        self.padding = 0.02
+
+        super().__init__(parent_window)
+
+
+    def draw_circle(self, center_x, center_y, radius, **kwargs):
+        x0 = center_x - radius
+        y0 = center_y - radius
+        x1 = center_x + radius
+        y1 = center_y + radius
+        return self.create_oval(x0, y0, x1, y1, **kwargs)
+
+    def draw_axis(self):
+        w = self.winfo_width()
+        a = self.padding * w
+
+        x0, y0 = w/4, w/4
+        r = w/4 - a
+        for n in range(1, 4):
+            self.draw_circle(x0, y0, r/3 * n, outline="#aaa",dash=(1, 3))
+        self.draw_circle(x0, y0, 0.9 * r, outline="#aaa",dash=(1,1)) # 3dB
+
+        x0, y0 = w*3/4, w/4
+        r = w/4 - a
+        for n in range(1, 4):
+            self.draw_circle(x0, y0, r/3 * n, outline="#aaa",dash=(1, 3))
+        self.draw_circle(x0, y0, 0.9 * r, outline="#aaa",dash=(1,1)) # 3dB
+
+        # axis
+        self.create_line(a, w/4, w/2 -a, w/4, fill='#aaa',dash=(1, 3))
+        self.create_line(w/4, a, w/4, w/2-a, fill='#aaa',dash=(1, 3))
+
+        self.create_line(w/2+a, w/4, w/2+w/2 -a, w/4, fill='#aaa',dash=(1, 3))
+        self.create_line(w/2+w/4, a, w/2+w/4, w/2-a, fill='#aaa',dash=(1, 3))
+
+    def draw(self, files):
+            self.delete("all")
+            l = files
+            colors = ['#000', '#f00', '#090', '#00f', '#990', '#099', '#f0f']
+            
+            self.draw_axis()
+            w = self.winfo_width()
+            a = self.padding * w
+
+            for i, msi_file in enumerate(l):
+                if pathlib.Path(msi_file).suffix.lower() == '.msi':
+                    color = colors[i % len(colors)]
+                    # draw pattern
+                    self.draw_pattern(msi_file, color)
+
+
+                    # draw antenna data
+                    header = read_header(msi_file)
+                    filename = pathlib.Path(msi_file).name
+                    header.insert(0, filename)
+
+                    # lines for one pattern
+                    if len(l) <= 1: 
+                        for k, line in enumerate(header):
+                            line = line.strip()
+                            if k==0:
+                                my_font = font.Font(family=self.fontname, size=self.fontsize, weight="bold")
+                            else:
+                                my_font = (self.fontname, self.fontsize)
+                            self.create_text(10, w/2 + k * self.fontsize * 1.5, text=line, fill=color, font=my_font, anchor='nw')
+
+                    # show 1 line for one paattern
+                    else:
+                        text = pathlib.Path(msi_file).name
+                        for k, line in enumerate(header):
+                            line = line.strip('\n').strip()
+                            if len(line) > 0:
+                                text = text + '|' + line.strip()
+                        self.create_text(10, w/2 + i * self.fontsize * 1.5, text=text, fill=color, font=(self.fontname, self.fontsize), anchor='nw')
+
+       
+    def draw_pattern(self, msi_file, color):
+
+        w = self.winfo_width()
+        a = self.padding * w
+        r0 = w/4 - a
+
+        dic = make_pattern_dic(msi_file)
+        points = dic['HORIZONTAL']
+        a = []
+        for point in points:
+            grd = point[0]
+            gain = point[1]
+            rad = grd / 360 * 2 * math.pi
+
+            r = r0 * (max(30 - gain, 0) / 30)
+            x = r * math.cos(rad) + w/4
+            y = r * math.sin(rad) + w/4
+            a.append(x)
+            a.append(y)
+        self.create_polygon(*a, fill='', outline=color)
+
+        points = dic['VERTICAL']
+        a = []
+        for point in points:
+            grd = point[0]
+            gain = point[1]
+            rad = grd / 360 * 2 * math.pi
+
+            r = r0 * (max(30 - gain, 0) / 30)
+            x = r * math.cos(rad) + w*3/4
+            y = r * math.sin(rad) + w/4
+            a.append(x)
+            a.append(y)
+        self.create_polygon(*a, fill='', outline=color)
+
+# ------------------- APP -------------------------------
 class App(tk.Tk):
     def __init__(self, start_msi):
         # ------ start argument ------
@@ -72,9 +278,7 @@ class App(tk.Tk):
 
         # ---- Settings ----
         self.start_geometry='1200x500'
-        self.fontname = "Consolas"
-        self.fontsize = 10
-        self.padding = 0.02
+
         self.ascending_0 = True
         self.ascending_1 = True
         self.ascending_2 = True
@@ -83,114 +287,33 @@ class App(tk.Tk):
         super().__init__()
         self.title('Patview')
         self.geometry(self.start_geometry)
-        self.bind("<Configure>", self.on_resize)
+        # self.bind("<Configure>", self.on_resize)
 
-        # --- Canvas ---
-        self.canvas = tk.Canvas(self, bg='white')
+        # --- Drawing ---
+        self.drawing = Drawing(self)
 
         # ---- frame for the left
         self.frame = ttk.Frame()
 
+        # ----------- FileList ---------
+        self.table = FileList(self, self.drawing)
+        self.table.get_files(self.root_folder)
+
         # --------- Browser1 -------------------
-        self.browser1 = ttk.Treeview(self.frame)
-        first_item = pathlib.Path(self.root_folder).resolve()
-
-
-        path_parts = self.root_folder.parts
-        item = ''
-        for i in range(1, len(path_parts) + 1):
-            full_path = pathlib.Path(*path_parts[0:i])
-            if  full_path.name == '':
-                entry_text = full_path
-            else:
-                entry_text = full_path.name
-            
-            item = self.browser1.insert(item, 'end', str(full_path), text=entry_text)
-            self.browser1.item(item, open=True)
-
-        self.browser1.bind('<<TreeviewOpen>>', self.add_sub_folders)
-        self.browser1.bind("<<TreeviewSelect>>", self.get_and_add_files)
-
-        # ------------ Browser1 ---------
-        self.browser1 = ttk.Treeview(self.frame)
-        first_item = pathlib.Path(self.root_folder).resolve()
-
-        path_parts = self.root_folder.parts
-        item = ''
-        for i in range(1, len(path_parts) + 1):
-            full_path = pathlib.Path(*path_parts[0:i])
-            if  full_path.name == '':
-                entry_text = full_path
-            else:
-                entry_text = full_path.name
-            
-            item = self.browser1.insert(item, 'end', str(full_path), text=entry_text)
-            self.browser1.item(item, open=True)
-
-        self.browser1.bind('<<TreeviewOpen>>', self.add_sub_folders)
-        self.browser1.bind("<<TreeviewSelect>>", self.get_and_add_files)
-
-        # select the last item
-        self.browser1.focus(item)              # Set keyboard focus
-        self.browser1.selection_set(item)      # Highlight it visually
-
-        # # Set focus and selection on the first item
-        self.add_sub_folders()
-
+        self.browser1 = FolderBrowser(self.frame, self.table)
+        self.browser1.set_folder(self.root_folder)
 
         # ------------ Browser2 ---------
-        self.browser2 = ttk.Treeview(self.frame)
-        first_item = pathlib.Path(self.root_folder).resolve()
-
-        path_parts = self.root_folder.parts
-        item = ''
-        for i in range(1, len(path_parts) + 1):
-            full_path = pathlib.Path(*path_parts[0:i])
-            if  full_path.name == '':
-                entry_text = full_path
-            else:
-                entry_text = full_path.name
-            
-            item = self.browser2.insert(item, 'end', str(full_path), text=entry_text)
-            self.browser2.item(item, open=True)
-
-        self.browser2.bind('<<TreeviewOpen>>', self.add_sub_folders)
-        self.browser2.bind("<<TreeviewSelect>>", self.get_and_add_files)
-
-        # select the last item
-        self.browser2.focus(item)              # Set keyboard focus
-        self.browser2.selection_set(item)      # Highlight it visually
-
-        # # Set focus and selection on the first item
-        self.add_sub_folders()
-
-
-        # --- Table ---
-        self.table = ttk.Treeview(self, columns=['freq', 'tilt'], selectmode="extended")
-        self.table.heading('#0', text='filename')
-        self.table.heading('freq', text='freq')
-        self.table.heading('tilt', text='tilt')
-        self.table.column('#0', width=300, anchor='w')
-        self.table.column('tilt', width=10, anchor='w')
-        self.table.column('freq', width=20, anchor='w')
-        self.table.bind("<<TreeviewSelect>>", self.draw)
-        self.table.bind('<Button-1>', self.on_treeview_click)
-
-        self.get_and_add_files()
- 
-
+        self.browser2 = FolderBrowser(self.frame, self.table)
+        self.browser2.set_folder(self.root_folder)
+   
         # --- Layout ----
-        self.browser1.pack(ipadx=100, expand=True, fill='both') #side='left', expand=False, fill='both', ipadx=100)
-        self.browser2.pack(ipadx=100, expand=True, fill='both') #side='left', expand=False, fill='both', ipadx=100)
+        self.browser1.pack(ipadx=100, expand=True, fill='both')
+        self.browser2.pack(ipadx=100, expand=True, fill='both') 
 
-        self.frame.pack(side='left', expand=True, fill='both')
-        self.table.pack(side='left', expand=False, fill='both', ipadx=50) # make it abit wider
-        self.canvas.pack(side='left', expand=True, fill='both')
-
-        # axis
-        self.draw_axis()
-
-
+        self.frame.pack(side='left', expand=False, fill='both')
+        self.table.pack(side='left', expand=False, fill='both', ipadx=50)
+        self.drawing.pack(side='left', expand=True, fill='both')
 
     def on_treeview_click(self, e):
         region = self.table.identify_region(e.x, e.y)
@@ -222,179 +345,14 @@ class App(tk.Tk):
 
             self.add_files()
 
-    def on_resize(self, e):
-        self.draw(e)
-    
+  
 
     def add_sub_folders(self, e=None):
         base = self.browser1.focus()
 
         for item in pathlib.Path(base).iterdir():
             if item.is_dir() and str(item) not in self.browser1.get_children(base):
-                self.browser1.insert(base, 'end', str(item), text=item.name)
-
-
-    def get_and_add_files(self, e=None):
-        base = self.browser1.focus()
-        
-         # add files
-        self.files = []
-        for item in pathlib.Path(base).iterdir():
-            if item.is_file():
-                item_str = str(item)
-
-                r = re.match(r'.*_(-\d|\d\d)T.*', item.name)
-                if r:
-                    tilt = r.group(1)
-                else:
-                    tilt = ''
-                r = re.match(r'.*_(\d{3,4})_', item.name)
-                if r:
-                    freq = r.group(1)
-                else:
-                    freq = ''
-
-                row = {
-                    'path' :str(item),
-                    'name' : item.name,
-                    'freq' : freq,
-                    'tilt' : tilt
-                }
-
-                self.files.append(row)
-
-        self.sort_files('name', ascending=True)
-        self.add_files()
-
-        try:
-            self.table.focus(self.start_msi)
-            self.table.selection_set(self.start_msi)
-            self.table.see(self.start_msi)
-        except:
-            print('not so good')
-
-    def sort_files(self, columns, ascending):
-        self.files = sorted(self.files, key=lambda row: row[columns], reverse= not ascending)
-
-    def add_files(self):
-
-       # delete all entries
-        for item in self.table.get_children():
-            self.table.delete(item)
-        # sort table
-        for row in self.files:        
-            self.table.insert('', 'end', row['path'], text=row['name'], values= [row['freq'], row['tilt'] ])
-
- 
-
-    def draw(self, e=None):
-        self.canvas.delete("all")
-        l = self.table.selection()
-        colors = ['#000', '#f00', '#090', '#00f', '#990', '#099', '#f0f']
-        
-        self.draw_axis()
-        w = self.canvas.winfo_width()
-        a = self.padding * w
-
-        # fill antenna data 
-        for i, msi_file in enumerate(l):
-            if pathlib.Path(msi_file).suffix.lower() == '.msi':
-                color = colors[i % len(colors)]
-                # draw pattern
-                self.draw_pattern(msi_file, color)
-
-
-                # draw antenna data
-                header = read_header(msi_file)
-                filename = pathlib.Path(msi_file).name
-                header.insert(0, filename)
-
-                # lines for one pattern
-                if len(l) <= 1: 
-                    for k, line in enumerate(header):
-                        line = line.strip()
-                        if k==0:
-                            my_font = font.Font(family=self.fontname, size=self.fontsize, weight="bold")
-                        else:
-                            my_font = (self.fontname, self.fontsize)
-                        self.canvas.create_text(10, w/2 + k * self.fontsize * 1.5, text=line, fill=color, font=my_font, anchor='nw')
-
-                # show 1 line for one paattern
-                else:
-                    text = pathlib.Path(msi_file).name
-                    for k, line in enumerate(header):
-                        line = line.strip('\n').strip()
-                        if len(line) > 0:
-                            text = text + '|' + line.strip()
-                    self.canvas.create_text(10, w/2 + i * self.fontsize * 1.5, text=text, fill=color, font=(self.fontname, self.fontsize), anchor='nw')
-
-
-
-    def draw_circle(self, center_x, center_y, radius, **kwargs):
-        x0 = center_x - radius
-        y0 = center_y - radius
-        x1 = center_x + radius
-        y1 = center_y + radius
-        return self.canvas.create_oval(x0, y0, x1, y1, **kwargs)
-
-    def draw_axis(self):
-        w = self.canvas.winfo_width()
-        a = self.padding * w
-
-        # circles
-        x0, y0 = w/4, w/4
-        r = w/4 - a
-        for n in range(1, 4):
-            self.draw_circle(x0, y0, r/3 * n, outline="#aaa",dash=(1, 3))
-        self.draw_circle(x0, y0, 0.9 * r, outline="#aaa",dash=(1,1)) # 3dB
-
-        x0, y0 = w*3/4, w/4
-        r = w/4 - a
-        for n in range(1, 4):
-            self.draw_circle(x0, y0, r/3 * n, outline="#aaa",dash=(1, 3))
-        self.draw_circle(x0, y0, 0.9 * r, outline="#aaa",dash=(1,1)) # 3dB
-
-        # axis
-        self.canvas.create_line(a, w/4, w/2 -a, w/4, fill='#aaa',dash=(1, 3))
-        self.canvas.create_line(w/4, a, w/4, w/2-a, fill='#aaa',dash=(1, 3))
-
-        self.canvas.create_line(w/2+a, w/4, w/2+w/2 -a, w/4, fill='#aaa',dash=(1, 3))
-        self.canvas.create_line(w/2+w/4, a, w/2+w/4, w/2-a, fill='#aaa',dash=(1, 3))
-       
-    def draw_pattern(self, msi_file, color):
-
-        w = self.canvas.winfo_width()
-        a = self.padding * w
-        r0 = w/4 - a
-
-        dic = make_pattern_dic(msi_file)
-        points = dic['HORIZONTAL']
-        a = []
-        for point in points:
-            grd = point[0]
-            gain = point[1]
-            rad = grd / 360 * 2 * math.pi
-
-            r = r0 * (max(30 - gain, 0) / 30)
-            x = r * math.cos(rad) + w/4
-            y = r * math.sin(rad) + w/4
-            a.append(x)
-            a.append(y)
-        self.canvas.create_polygon(*a, fill='', outline=color)
-
-        points = dic['VERTICAL']
-        a = []
-        for point in points:
-            grd = point[0]
-            gain = point[1]
-            rad = grd / 360 * 2 * math.pi
-
-            r = r0 * (max(30 - gain, 0) / 30)
-            x = r * math.cos(rad) + w*3/4
-            y = r * math.sin(rad) + w/4
-            a.append(x)
-            a.append(y)
-        self.canvas.create_polygon(*a, fill='', outline=color)
+                self.browser1.insert(base, 'end', str(item), text=item.name)  
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
