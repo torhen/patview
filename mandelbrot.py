d37ab29cf36a1c3e2d6469b6674b09558b3bbf53
torhen <torhen@gmail.com>
2026-05-31 11:57:24 +0200

    Add files via upload

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,23 +1,47 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import matplotlib.colors as colors
+from numba import njit, prange
 
 
-def make_array():
-    x = np.linspace(g_x0, g_x1, 300)
-    y = np.linspace(g_y0, g_y1, 300)
-    m = np.meshgrid(x, y)
+# def make_array(x0, x1, y0, y1):
+#     width = 500
+#     x = np.linspace(x0, x1, width)
+#     y = np.linspace(y0, y1, width)
+#     m = np.meshgrid(x, y)
 
-    C = m[0] + 1j * m[1]
-    X = np.zeros_like(C)
-    I = np.full_like(C, -1, dtype=int)
+#     C = m[0] + 1j * m[1]
+#     X = np.zeros_like(C)
+#     I = np.full_like(C, -1, dtype=int)
 
-    for i in range(100):
-        X = X**2 + C
-        I[(abs(X) > 2) & (I < 0)] = i
+#     for i in range(100):
+#         X = X**2 + C
+#         I[(abs(X) > 2) & (I < 0)] = i
+
+#     return I
+
+
+@njit(parallel=True)
+def make_array(x0, x1, y0, y1):
+    width = 500
+    x = np.linspace(x0, x1, width)
+    y = np.linspace(y0, y1, width)
+
+    I = np.full((width, width), -1, dtype=np.int32)
+
+    for iy in prange(width):
+        for ix in range(width):
+            cx = x[ix]
+            cy = y[iy]
+            c = complex(cx, cy)
+            z = 0j
+
+            for i in range(100):
+                z = z*z + c
+                if abs(z) > 2:
+                    I[iy, ix] = i
+                    break
 
     return I
-
 
 def on_button_press(event):
     if event.xdata is None or event.ydata is None:
@@ -61,7 +85,7 @@ def on_scroll(event):
     update()
 
 def update():
-    arr = make_array()
+    arr = make_array(g_x0, g_x1, g_y0, g_y1)
     im.set_data(arr)
     im.set_clim(arr.min(), arr.max())
     im.set_extent((g_x0, g_x1, g_y0, g_y1))
@@ -78,8 +102,9 @@ fig.canvas.mpl_connect("scroll_event", on_scroll)
 
 ax = fig.add_subplot()
 ax.grid()
-arr = make_array()
+arr = make_array(g_x0, g_x1, g_y0, g_y1)
 im = ax.imshow(arr, extent=(g_x0, g_x1, g_y0, g_y1), cmap='gist_ncar', origin='lower')
+# fig.colorbar()
 plt.show()
 
 
